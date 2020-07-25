3a3fb2fe384f16e6f71da1c759c368f7ba4966fb
Iago Marinheiro Elias <go.marinheiro@gmail.com>
2020-07-25 00:58:16 -0300

    :sparkles: home page

--- SAES/SAES/urls.py
+++ SAES/SAES/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from appsaes.views.encrypt_view import EncryptView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', EncryptView.as_view(), name='home'),
+    path(r'', TemplateView.as_view(template_name="home.html"), name="home"),
     path(r'', include('appsaes.urls')),
     path(r'rsa/', include('apprsa.urls')),
 ]
